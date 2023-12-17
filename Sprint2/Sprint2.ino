931371a2f411384fcd81243b9c0c49f87e5d19dd
Josue Bellota <69655184+Jhosua1769@users.noreply.github.com>
2023-12-17 20:51:20 +0100

    Update Sprint2.ino

--- Sprint2/Sprint2.ino
+++ Sprint2/Sprint2.ino
@@ -1,103 +1,111 @@
 #include <Wire.h>
 #include <Adafruit_ADS1X15.h>
 
+Adafruit_ADS1115 ads;
+
 #define power_pin 5
-#define channelValue 0
-#define offset 0.4
+#define offset 0
 #define samplingInterval 20
 #define printInterval 800
-#define ArrayLength 40 // numero de muestras
+#define ArrayLength 40
 
-const int n = 5;
-double valorDigital[n] = {519, 654, 666, 670, 674};
-double salinidad[n] = {0, 10, 15, 20, 25};
-
-// Function prototypes
-void salinidadSetup();
-void salinidadLoop();
-void pHSetup();
-void pHLoop();
+int pHArray[ArrayLength]; // almacena las muestras
+int pHArrayIndex = 0;
 
 void setup() {
-  salinidadSetup();
-  pHSetup();
+  Serial.begin(9600);
+  ads.begin();
+  ads.setGain(GAIN_ONE);
+  pinMode(power_pin, OUTPUT);
+  Serial.println("Inicializando el medidor de pH");
 }
 
 void loop() {
-  salinidadLoop();
-  pHLoop();
+  // int16_t humiditySensorValue = ads.readADC_SingleEnded(humidity_channel);
+  // int16_t temperatureSensorValue = ads.readADC_SingleEnded(temperature_channel);
+  // int16_t salinitySensorValue = ads.readADC_SingleEnded(salinity_channel);
+  // int16_t pHSensorValue = ads.readADC_SingleEnded(pH_channel);
+  int16_t sensorValue = ads.readADC_SingleEnded(0);
+
+  // calculateAndPrintHumidity(sensorValue);
+  // calculateAndPrintTemperature(sensorValue);
+  readAndCalculateSalinity();
+  checkLightLevel(sensorValue);
+  // readAndCalculatepH();
+  delay(1000);
 }
 
-void salinidadSetup() {
-  pinMode(power_pin, OUTPUT);
-  Serial.begin(9600);
+void calculateAndPrintHumidity(int16_t sensorValue) {
+  int humidityValue = map(sensorValue, 30123, 17290, 0, 100);
+  Serial.println();
+  Serial.print("Humedad: ");
+  Serial.print(humidityValue);
+  Serial.println("%");
+}
+
+void calculateAndPrintTemperature(int16_t sensorValue) {
+  float v0 = (sensorValue * 4.096) / 32767;
+  float T = (v0 - 0.79) / 0.035;
+  Serial.print(T);
+  Serial.println(" Celsius");
 }
 
 double lagrangeInterpolation(double* x, double* y, int n, double xi) {
-    double result = 0.0;
+  double result = 0.0;
 
-    for (int i = 0; i < n; i++) {
-        double term = y[i];
-        for (int j = 0; j < n; j++) {
-            if (j != i) {
-                term = term * (xi - x[j]) / (x[i] - x[j]);
-            }
-        }
-        result += term;
+  for (int i = 0; i < n; i++) {
+    double term = y[i];
+    for (int j = 0; j < n; j++) {
+      if (j != i) {
+        term = term * (xi - x[j]) / (x[i] - x[j]);
+      }
     }
+    result += term;
+  }
 
-    return result;
+  return result;
 }
 
-void salinidadLoop() {
+void readAndCalculateSalinity() {
+  const int n = 5;
+  double valorDigital[n] = {519, 654, 666, 670, 674};
+  double salinidad[n] = {0, 10, 15, 20, 25};
   int16_t adc0;
 
-  // Alimentamos la sonda con un tren de pulsos
   digitalWrite(power_pin, HIGH);
   delay(100);
 
-  // Leemos cuando hay un nivel alto
-  // adc0 = analogRead(0);
-  adc0 = 639;
-
-  // Calculamos la salinidad usando interpolación de Lagrange
-  int16_t Salinidad = lagrangeInterpolation(valorDigital, salinidad, n, adc0);
+  adc0 = ads.readADC_SingleEnded(0);
 
   digitalWrite(power_pin, LOW);
   delay(100);
+  
+  int16_t Salinidad = lagrangeInterpolation(valorDigital, salinidad, n, adc0);
 
   Serial.print("Salinidad (g): ");
-  Serial.println(Salinidad);
-  Serial.println(adc0);
+  Serial.println(Salinidad); 
+  Serial.println(adc0); 
 }
 
-void pHSetup() {
-  // Inicializamos el monitor serie
-  Serial.begin(9600);
-  Serial.println("Inicializando el medidor de pH");
-
-  // Inicializamos el ADS1115
-  ads.begin();
-
-  // Configuramos la ganancia del ADS1115
-  ads.setGain(GAIN_ONE);
-}
-
-void pHLoop() {
+void readAndCalculatepH() {
   static unsigned long samplingTime = millis();
   static unsigned long printTime = millis();
   static float pHValue, voltage;
   int16_t adc;
 
-  if (millis() - samplingTime > samplingInterval) { // Cada samplingTime segundos se toma una muestra
-    adc = ads.readADC_SingleEnded(channelValue);
+  if (millis() - samplingTime > samplingInterval) {
+    adc = ads.readADC_SingleEnded(0);
     voltage = adc * 4.096 / 32767;
-    pHValue = 3.5 * voltage + offset;
+    pHValue = ((3.5 * voltage) + offset);
+
+
+    double adcv[n] = {1.77, 654, 666, 670, 674};
+    double phv[n] = {4.0, 10, 15, 20, 25};
 
     Serial.print("adc: ");
-    Serial.print(adc, DEC);
-    Serial.print(" Voltage: ");
-    Serial.print(voltage, 2);
+    Serial.println(adc, DEC);
+    Serial.print("Voltage: ");
+    Serial.println(voltage, 2);
     Serial.print(" pH value: ");
     Serial.println(pHValue, 2);
 
@@ -108,8 +116,21 @@ void pHLoop() {
     samplingTime = millis();
   }
 
-  if (millis() - printTime >= 1000) {
-    // Tu código que se ejecutará si la condición es verdadera
-    printTime = millis(); // Actualiza el tiempo de la última ejecución
+  if (millis() - printTime >= 10000) {
+    // Your code to execute if the condition is true
+    printTime = millis();
   }
 }
+
+void checkLightLevel(int16_t sensorValue) {
+  if (sensorValue < UMBRAL_OSCURO) {
+    Serial.println("Oscuridad");
+  } else if (sensorValue < UMBRAL_SOMBRA) {
+    Serial.println("Sombra");
+  } else if (sensorValue < UMBRAL_LUZ_AMBIENTE) {
+    Serial.println("Luz ambiente");
+  } else {
+    Serial.println("Nivel alto de iluminación");
+  }
+}
+
